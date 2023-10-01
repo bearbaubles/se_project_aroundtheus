0cb02ddb13a10c6e135ee46e4cf6a5ee23f84130
Kain <giblets806@gmail.com>
2023-10-01 14:58:19 -0500

    task 1

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  (garden = {
+    name: "Garden of the Gods",
+    link: "https://unsplash.com/photos/dDWLTcR9Gpc",
+  }),
+  (disneyWorld = {
+    name: "Disney World",
+    link: "https://unsplash.com/photos/FAfwbKD77SU",
+  }),
+  (hudsonRiver = {
+    name: "Hudson River",
+    link: "https://unsplash.com/photos/xi-37Un_GjA",
+  }),
+  (alaskaBorealis = {
+    name: "Aurora Borealis: Alaska",
+    link: "https://unsplash.com/photos/7X1LwOfrRrc",
+  }),
+  (newYork = {
+    name: "New York City",
+    link: "https://unsplash.com/photos/rfj_wOYQkus",
+  }),
+  (cathedral = {
+    name: "Cathedral of St. Helena",
+    link: "https://unsplash.com/photos/T5n0soOjqu4",
+  }),
+];
